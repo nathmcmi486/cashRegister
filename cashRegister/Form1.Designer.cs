6293cc7de3cb440494190e425201f8653d865e68
Nathan McMillan <nathmcmi486@ed.amdsb.ca>
2023-02-21 13:45:18 -0500

    Add project files.

---
+++ cashRegister/Form1.Designer.cs
@@ -0,0 +1,86 @@
+﻿namespace cashRegister
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.title = new System.Windows.Forms.Label();
+            this.mainButton = new System.Windows.Forms.Button();
+            this.amountInput = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.title.AutoSize = true;
+            this.title.Location = new System.Drawing.Point(150, 68);
+            this.title.Name = "title";
+            this.title.Size = new System.Drawing.Size(35, 13);
+            this.title.TabIndex = 0;
+            this.title.Text = "The Cash Register";
+            // 
+            // button1
+            // 
+            this.mainButton.Location = new System.Drawing.Point(78, 130);
+            this.mainButton.Name = "button1";
+            this.mainButton.Size = new System.Drawing.Size(75, 23);
+            this.mainButton.TabIndex = 1;
+            this.mainButton.Text = "button1";
+            this.mainButton.UseVisualStyleBackColor = true;
+            this.mainButton.Click += new System.EventHandler(this.mainButton_Click);
+            // 
+            // textBox1
+            // 
+            this.amountInput.Location = new System.Drawing.Point(22, 225);
+            this.amountInput.Name = "textBox1";
+            this.amountInput.Size = new System.Drawing.Size(100, 20);
+            this.amountInput.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(373, 450);
+            this.Controls.Add(this.amountInput);
+            this.Controls.Add(this.mainButton);
+            this.Controls.Add(this.title);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label title;
+        private System.Windows.Forms.Button mainButton;
+        private System.Windows.Forms.TextBox amountInput;
+    }
+}
+
