ca68f0463db144949bc168d24a46bf4297567811
Nathan McMillan <nathmcmi486@ed.amdsb.ca>
2023-02-22 14:49:35 -0500

    Worked on stuff

--- cashRegister/Form1.Designer.cs
+++ cashRegister/Form1.Designer.cs
@@ -2,6 +2,8 @@
 {
     partial class Form1
     {
+
+
         /// <summary>
         /// Required designer variable.
         /// </summary>
@@ -29,11 +31,22 @@
         private void InitializeComponent()
         {
             this.title = new System.Windows.Forms.Label();
-            this.mainButton = new System.Windows.Forms.Button();
+            this.calcOutputLabel = new System.Windows.Forms.Label();
+            this.calculateTotal = new System.Windows.Forms.Button();
             this.amountInput = new System.Windows.Forms.TextBox();
+            this.calculateChangeBtn = new System.Windows.Forms.Button();
+
+            this.frenchFryLabel = new System.Windows.Forms.Label();
+            this.cheeseBurgerLabel = new System.Windows.Forms.Label();
+            this.waterLabel = new System.Windows.Forms.Label();
+
+            this.fryInput = new System.Windows.Forms.TextBox();
+            this.cheeseBurgerInput = new System.Windows.Forms.TextBox();
+            this.waterInput = new System.Windows.Forms.TextBox();
+
             this.SuspendLayout();
             // 
-            // label1
+            // Title label
             // 
             this.title.AutoSize = true;
             this.title.Location = new System.Drawing.Point(150, 68);
@@ -41,21 +54,67 @@
             this.title.Size = new System.Drawing.Size(35, 13);
             this.title.TabIndex = 0;
             this.title.Text = "The Cash Register";
+
+            this.calcOutputLabel.AutoSize = true;
+            this.calcOutputLabel.Location = new System.Drawing.Point(50, 185);
+            this.calcOutputLabel.Name = "calcOutputLabel";
+            this.calcOutputLabel.Size = new System.Drawing.Size(35, 13);
+            this.calcOutputLabel.TabIndex = 0;
+            this.calcOutputLabel.Text = "";
+
+            // Item labels
+            this.frenchFryLabel.AutoSize = true;
+            this.frenchFryLabel.Location = new System.Drawing.Point(50, 90);
+            this.frenchFryLabel.Name = "frenchFryLabel";
+            this.frenchFryLabel.Size = new System.Drawing.Size(35, 13);
+            this.frenchFryLabel.TabIndex = 0;
+            this.frenchFryLabel.Text = $"French Fries {FRENCH_FRY_COST.ToString("C")}";
+
+            this.cheeseBurgerLabel.AutoSize = true;
+            this.cheeseBurgerLabel.Location = new System.Drawing.Point(50, 110);
+            this.cheeseBurgerLabel.Name = "cheeseBurgerLabel";
+            this.cheeseBurgerLabel.Size = new System.Drawing.Size(35, 13);
+            this.cheeseBurgerLabel.TabIndex = 0;
+            this.cheeseBurgerLabel.Text = $"Cheese Burgers {CHEESE_BURGER_COST.ToString("C")}";
+
+            this.waterLabel.AutoSize = true;
+            this.waterLabel.Location = new System.Drawing.Point(50, 128);
+            this.waterLabel.Name = "waterLabel";
+            this.waterLabel.Size = new System.Drawing.Size(35, 13);
+            this.waterLabel.TabIndex = 0;
+            this.waterLabel.Text = $"Water {WATER_COST.ToString("C")}";
+
+            // Item input boxes
+            this.fryInput.Location = new System.Drawing.Point(170, 90);
+            this.fryInput.Name = "fryInput";
+            this.fryInput.Size = new System.Drawing.Size(100, 20);
+            this.fryInput.TabIndex = 2;
+
+            this.cheeseBurgerInput.Location = new System.Drawing.Point(170, 112);
+            this.cheeseBurgerInput.Name = "cheeseBurgerInput";
+            this.cheeseBurgerInput.Size = new System.Drawing.Size(100, 20);
+            this.cheeseBurgerInput.TabIndex = 2;
+
+            this.waterInput.Location = new System.Drawing.Point(170, 134);
+            this.waterInput.Name = "waterInput";
+            this.waterInput.Size = new System.Drawing.Size(100, 20);
+            this.waterInput.TabIndex = 2;
+
             // 
-            // button1
+            // "Main" (calculate) button
             // 
-            this.mainButton.Location = new System.Drawing.Point(78, 130);
-            this.mainButton.Name = "button1";
-            this.mainButton.Size = new System.Drawing.Size(75, 23);
-            this.mainButton.TabIndex = 1;
-            this.mainButton.Text = "button1";
-            this.mainButton.UseVisualStyleBackColor = true;
-            this.mainButton.Click += new System.EventHandler(this.mainButton_Click);
+            this.calculateTotal.Location = new System.Drawing.Point(50, 155);
+            this.calculateTotal.Name = "calculateTotal";
+            this.calculateTotal.Size = new System.Drawing.Size(75, 23);
+            this.calculateTotal.TabIndex = 1;
+            this.calculateTotal.Text = "Calculate total";
+            this.calculateTotal.UseVisualStyleBackColor = true;
+            this.calculateTotal.Click += new System.EventHandler(this.calculateTotal_Click);
             // 
-            // textBox1
+            // Input amount
             // 
             this.amountInput.Location = new System.Drawing.Point(22, 225);
-            this.amountInput.Name = "textBox1";
+            this.amountInput.Name = "amountInput";
             this.amountInput.Size = new System.Drawing.Size(100, 20);
             this.amountInput.TabIndex = 2;
             // 
@@ -65,22 +124,39 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(373, 450);
             this.Controls.Add(this.amountInput);
-            this.Controls.Add(this.mainButton);
+            this.Controls.Add(this.calculateTotal);
             this.Controls.Add(this.title);
+            this.Controls.Add(this.calcOutputLabel);
+
+            // Add item labels
+            this.Controls.Add(this.frenchFryLabel);
+            this.Controls.Add(this.cheeseBurgerLabel);
+            this.Controls.Add(this.waterLabel);
+
+            // Add item inputs
+            this.Controls.Add(this.fryInput);
+            this.Controls.Add(this.cheeseBurgerInput);
+            this.Controls.Add(this.waterInput);
+
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
             this.PerformLayout();
-
-
-
         }
 
         #endregion
 
         private System.Windows.Forms.Label title;
-        private System.Windows.Forms.Button mainButton;
+        private System.Windows.Forms.Button calculateTotal;
+        private System.Windows.Forms.Button calculateChangeBtn;
         private System.Windows.Forms.TextBox amountInput;
+        private System.Windows.Forms.TextBox fryInput;
+        private System.Windows.Forms.TextBox cheeseBurgerInput;
+        private System.Windows.Forms.TextBox waterInput;
+        private System.Windows.Forms.Label frenchFryLabel;
+        private System.Windows.Forms.Label cheeseBurgerLabel;
+        private System.Windows.Forms.Label waterLabel;
+        private System.Windows.Forms.Label calcOutputLabel;
     }
 }
 
--- cashRegister/Form1.cs
+++ cashRegister/Form1.cs
@@ -12,14 +12,69 @@ namespace cashRegister
 {
     public partial class Form1 : Form
     {
+        // Foos costs
+        public const double FRENCH_FRY_COST = 1.99;
+        public const double CHEESE_BURGER_COST = 3.5;
+        public const double WATER_COST = 9.99;
+
+        /* 
+         * Index 0 - Frenh fires
+         * Index 1 - Cheese burgers
+         * Index 2 - Water
+        */
+        public static uint[] QUANTITY = {0, 0, 0};
+        // 
+        private static double AMOUNT = 0.0;
+        private static double PAID_AMOUNT = 0.0;
+        private static double TAX_AMOUNT = 0.0;
+        public const double TAX_RATE = 0.13;
+        
+
         public Form1()
         {
             InitializeComponent();
         }
 
-        private void mainButton_Click(object _, EventArgs e)
+        /* ``calculateTotal_Click(object, EventArgs)``
+         * _ - Unused
+         * e - Unused
+         * 
+         * Calculates the amount (``AMOUNT``) with tax (``TAX_AMOUNT``) from the values in ``QUANTITY``.
+        */
+        private void calculateTotal_Click(object _, EventArgs e)
         {
+            try
+            {
+                QUANTITY[0] = Convert.ToUInt32(this.fryInput.Text);
+                QUANTITY[1] = Convert.ToUInt32(this.cheeseBurgerInput.Text);
+                QUANTITY[2] = Convert.ToUInt32(this.waterInput.Text);
+            } catch
+            {
+                System.Console.WriteLine("There was an invalid input");
+                this.calcOutputLabel.Text = "Please enter valid numbers in all text boxes";
+                return;
+            }
 
+            double amount = (QUANTITY[0] * FRENCH_FRY_COST) + (QUANTITY[1] * CHEESE_BURGER_COST) + (QUANTITY[2] * WATER_COST);
+            double tax_amount = amount * TAX_RATE;
+            amount = amount + tax_amount;
+            System.Console.WriteLine($"Amount: {amount}");
+            AMOUNT = amount;
+            TAX_AMOUNT = tax_amount;
+            this.calcOutputLabel.Text = $"Total amount: {AMOUNT}";
+        }
+
+        private void calculateChange(object _, EventArgs e)
+        {
+            try
+            {
+                PAID_AMOUNT = Convert.ToDouble(this.amountInput.Text);
+            }
+            catch
+            {
+                this.Controls.Remove(this.amountInput);
+                throw;
+            }
         }
     }
 }
